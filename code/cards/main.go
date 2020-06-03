559633b89379ddf5c26ff656a42a7d85aa0ebe2a
Jerry Cason <jcason@protonmail.ch>
2020-06-03 16:12:53 -0400

    Added the dealing functionality to display players hand

--- code/cards/main.go
+++ code/cards/main.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 	cards.shuffle()
+	hand, cards := deal(cards, 2)
+	fmt.Println("Your hand contains: ")
+	hand.print()
+	fmt.Println("Cards left in the deck are: ")
 	cards.print()
 }
